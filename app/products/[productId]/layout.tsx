97c922ec26c3748089884d501fbc83eca38bde4a
Gerald Joaquin <geraldsantosjoaquin1@gmail.com>
2026-07-16 18:40:39 -0700

    Nested Layout

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,8 @@
+export default function productDetailsLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main>
+            {children}
+            <h1>Product Details Layout</h1> 
+        </main>
+    )
+}
