08aacdf73e7a6d90eacd26b3e3cdf385dc8ad33a
Gerald Joaquin <geraldsantosjoaquin1@gmail.com>
2026-07-17 13:35:33 +0800

    Active Link Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/about">About</Link>
     </main>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function productList (){
     return (
         <main>
             <h1>Product List Page!</h1>
-            <h3>Product 1</h3>
-            <h3>Product 2</h3>
-            <h3>Product 3</h3>
+            <Link href="/products/1">Product 1</Link>
+            <Link href="/products/2">Product 2</Link>
+            <Link href="/products/3">Product 3</Link>
         </main>
     )
 }
