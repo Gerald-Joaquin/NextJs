84cba788333b0613118e79206ddca719c126eac5
Gerald Joaquin <geraldsantosjoaquin1@gmail.com>
2026-07-17 13:21:08 +0800

    Metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,4 +1,23 @@
-export default function productDetail () {
+import { Metadata } from 'next';
+
+type Props = {
+    params: Promise<{ productId: string }>;
+}
+
+export const generatedMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Product: ${id}`);
+        }, 1000);
+    });
+    return {
+        title: `Product: ${title}`,
+    };
+};
+
+export default async function productDetail ({params}: Props) {
+    const productId = (await params) .productId;
     return (
         <main>
             <h1>Product Detail Page!</h1>  
